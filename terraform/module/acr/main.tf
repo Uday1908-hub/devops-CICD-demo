4816a66b5dfaa12861f733fd1c26d6c5d83307a0
Uday1908-hub <Uday.Kiran.S@gds.ey.com>
2026-04-27 12:55:59 +0530

    Update main.tf

--- terraform/module/acr/main.tf
+++ terraform/module/acr/main.tf
@@ -7,28 +7,6 @@ resource "azurerm_container_registry" "this" {
 }
 
 
-output "acr_id" {
-  value = azurerm_container_registry.this.id
-}
-
-output "acr_name" {
-  value = azurerm_container_registry.this.name
-}
-
-output "acr_login_server" {
-  value = azurerm_container_registry.this.login_server
-}
 
 
-variable "acr_name" {
-  type = string
-}
-
-variable "resource_group_name" {
-  type = string
-}
-
-variable "location" {
-  type = string
-}
 
