237e5e39f10d486ab83272aede930f54c094a3c3
Uday1908-hub <Uday.Kiran.S@gds.ey.com>
2026-04-27 12:58:13 +0530

    Create variables.tf

---
+++ terraform/module/aks/variables.tf
@@ -0,0 +1,26 @@
+
+variable "aks_cluster_name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "node_count" {
+  type = number
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "acr_id" {
+  type = string
+}
+
+
