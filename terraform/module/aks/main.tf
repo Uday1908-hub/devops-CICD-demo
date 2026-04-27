78f5aeb44747d2212f273cbe71b829ccb6f0c93b
Uday1908-hub <Uday.Kiran.S@gds.ey.com>
2026-04-27 12:05:06 +0530

    Create main.tf

---
+++ terraform/module/aks/main.tf
@@ -0,0 +1,62 @@
+resource "azurerm_kubernetes_cluster" "this" {
+  name                = var.aks_cluster_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = var.aks_cluster_name
+
+  default_node_pool {
+    name       = "system"
+    node_count = var.node_count
+    vm_size    = "Standard_B2s_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  oidc_issuer_enabled = true
+
+  tags = {
+    environment = var.environment
+    workload    = "cloudforge-api"
+  }
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = var.acr_id
+  skip_service_principal_aad_check = true
+}
+
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.this.name
+}
+
+
+variable "aks_cluster_name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "node_count" {
+  type = number
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "acr_id" {
+  type = string
+}
+
+
