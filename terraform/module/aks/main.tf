08b9677f8913f45aac4459caf908f57ea8cbd811
Uday1908-hub <Uday.Kiran.S@gds.ey.com>
2026-04-27 12:57:51 +0530

    Update main.tf

--- terraform/module/aks/main.tf
+++ terraform/module/aks/main.tf
@@ -32,29 +32,3 @@ resource "azurerm_role_assignment" "acr_pull" {
 
 
 
-
-variable "aks_cluster_name" {
-  type = string
-}
-
-variable "resource_group_name" {
-  type = string
-}
-
-variable "location" {
-  type = string
-}
-
-variable "node_count" {
-  type = number
-}
-
-variable "environment" {
-  type = string
-}
-
-variable "acr_id" {
-  type = string
-}
-
-
