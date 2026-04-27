ececb37726b9fd55abe568095f4f8402ec7b3911
Uday1908-hub <Uday.Kiran.S@gds.ey.com>
2026-04-27 13:00:36 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,22 +22,6 @@ module "aks" {
 }
 
 
-output "resource_group_name" {
-  value = module.resource_group.resource_group_name
-}
-
-output "acr_name" {
-  value = module.acr.acr_name
-}
-
-output "acr_login_server" {
-  value = module.acr.acr_login_server
-}
-
-output "aks_cluster_name" {
-  value = module.aks.aks_cluster_name
-}
-
 
 terraform {
   required_version = ">= 1.5.0"
